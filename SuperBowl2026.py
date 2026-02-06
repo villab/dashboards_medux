bd92b5d8890d5b4aec2d9608c1b41e0c47ffabcd
villab <cevillate@gmail.com>
2026-02-06 17:46:50 -0500

    Update SuperBowl2026.py

--- SuperBowl2026.py
+++ SuperBowl2026.py
@@ -694,6 +694,20 @@ def grafica_kpi(df, y_field, titulo, freq="5min", agg_func="mean",color_by="isp"
             tickformat=".0%",
             range=[0, 1.01]    
         )
+
+
+    if color_by == "target":
+    fig.update_layout(
+        legend=dict(
+            orientation="h",
+            yanchor="bottom",
+            y=1.12,
+            xanchor="center",
+            x=0.5,
+            title_text=""
+        )
+    )
+
     st.plotly_chart(fig, use_container_width=True)
 
 
