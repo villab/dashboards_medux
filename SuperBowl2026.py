7ee6f2b1177674a211e2b84134062f4f29e8a589
villab <cevillate@gmail.com>
2026-02-06 16:29:12 -0500

    Update SuperBowl2026.py

--- SuperBowl2026.py
+++ SuperBowl2026.py
@@ -727,7 +727,7 @@ else:
     df_voice = df_kpi[df_kpi["test"] == "voice-out"]
     if not df_voice.empty:
         grafica_kpi(df_voice, "callSetUpTimeL3", "Call set up time (ms)")
-        df_voice["callSetUpSuccess %"] = df_voice_set["callSetUpSuccessL3"] * 100
+        df_voice["callSetUpSuccess %"] = df_voice["callSetUpSuccessL3"] * 100
 
         grafica_kpi(df_voice, "callSetUpSuccessL3", "Call set up success (%)")
 
